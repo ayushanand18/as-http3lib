3cbad982b009e1990c50f674cf480d2b4feee150
Ayush Anand <36472216+ayushanand18@users.noreply.github.com>
2024-01-21 11:23:58 +0000

    update: writes unit tests for testing out the library

---
+++ tests/test_h1.cpp
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+#include "../ashttp3lib/h1/request.hpp"
+
+// Test case for ashttp3lib::h1::utils::split() function
+TEST(AsHttp3LibTest, SplitFunction) {
+    std::vector<std::string> run_result;
+    std::vector<std::string> expected_result {"hello", "world"};
+
+    run_result = ashttp3lib::h1::utils::split("hello world", " ");
+
+    // Assert
+    ASSERT_EQ(run_result, expected_result);
+}
+
+// Test case for request parsing by ashttp3lib::h1::request::Request Class
+TEST(AsHttp3LibTest, RequestParsing) {
+    std::string input_stream = "GET /test HTTP/1.1\r\nHost: example.com\r\nUser-Agent: Your-User-Agent\r\nAccept: */*\r\n\r\n";
+    std::istringstream request_stream(input_stream);
+    ashttp3lib::h1::Request result(request_stream);
+
+    // Assert, all headers are parsed directly
+    ASSERT_EQ(result.headers.size(), 3);
+    // Assert, method is as passed
+    ASSERT_EQ(result.method, "GET");
+    // Assert, path is as passed
+    ASSERT_EQ(result.path, "/test");
+    // Assert, pick a random header 
+    ASSERT_EQ(result.headers.at("Host"), "example.com");
+}
