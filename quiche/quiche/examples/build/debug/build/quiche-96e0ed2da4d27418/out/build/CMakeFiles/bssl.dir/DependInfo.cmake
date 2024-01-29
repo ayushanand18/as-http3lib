78a465492fb3aede0eb8c2658be18b676c75f325
Ayush Anand <36472216+ayushanand18@users.noreply.github.com>
2024-01-29 17:12:33 +0000

    update integrates cloudflare/quiche with c++ apis

---
+++ quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/bssl.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/tool/args.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/bssl.dir/src/tool/args.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/tool/ciphers.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/bssl.dir/src/tool/ciphers.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/tool/client.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/bssl.dir/src/tool/client.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/tool/const.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/bssl.dir/src/tool/const.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/tool/digest.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/bssl.dir/src/tool/digest.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/tool/fd.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/bssl.dir/src/tool/fd.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/tool/file.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/bssl.dir/src/tool/file.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/tool/generate_ech.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/bssl.dir/src/tool/generate_ech.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/tool/generate_ed25519.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/bssl.dir/src/tool/generate_ed25519.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/tool/genrsa.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/bssl.dir/src/tool/genrsa.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/tool/pkcs12.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/bssl.dir/src/tool/pkcs12.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/tool/rand.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/bssl.dir/src/tool/rand.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/tool/server.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/bssl.dir/src/tool/server.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/tool/sign.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/bssl.dir/src/tool/sign.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/tool/speed.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/bssl.dir/src/tool/speed.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/tool/tool.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/bssl.dir/src/tool/tool.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/tool/transport_common.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/bssl.dir/src/tool/transport_common.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BORINGSSL_IMPLEMENTATION"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/DependInfo.cmake"
+  "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/crypto.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/DependInfo.cmake
@@ -0,0 +1,62 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/bio_ssl.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/bio_ssl.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/d1_both.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/d1_both.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/d1_lib.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/d1_lib.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/d1_pkt.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/d1_pkt.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/d1_srtp.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/d1_srtp.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/dtls_method.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/dtls_method.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/dtls_record.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/dtls_record.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/encrypted_client_hello.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/encrypted_client_hello.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/extensions.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/extensions.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/handoff.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/handoff.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/handshake.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/handshake.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/handshake_client.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/handshake_client.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/handshake_server.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/handshake_server.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/s3_both.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/s3_both.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/s3_lib.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/s3_lib.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/s3_pkt.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/s3_pkt.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/ssl_aead_ctx.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/ssl_aead_ctx.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/ssl_asn1.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/ssl_asn1.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/ssl_buffer.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/ssl_buffer.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/ssl_cert.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/ssl_cert.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/ssl_cipher.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/ssl_cipher.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/ssl_file.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/ssl_file.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/ssl_key_share.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/ssl_key_share.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/ssl_lib.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/ssl_lib.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/ssl_privkey.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/ssl_privkey.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/ssl_session.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/ssl_session.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/ssl_stat.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/ssl_stat.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/ssl_transcript.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/ssl_transcript.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/ssl_versions.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/ssl_versions.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/ssl_x509.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/ssl_x509.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/t1_enc.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/t1_enc.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/tls13_both.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/tls13_both.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/tls13_client.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/tls13_client.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/tls13_enc.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/tls13_enc.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/tls13_server.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/tls13_server.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/tls_method.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/tls_method.cc.o"
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/ssl/tls_record.cc" "/workspaces/as-http3lib/quiche/quiche/examples/build/debug/build/quiche-96e0ed2da4d27418/out/build/CMakeFiles/ssl.dir/src/ssl/tls_record.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BORINGSSL_IMPLEMENTATION"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/workspaces/as-http3lib/quiche/quiche/deps/boringssl/src/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
