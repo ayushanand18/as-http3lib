19c7768becbf1c40105f46238e1775c13e8fc4fe
Ayush Anand <36472216+ayushanand18@users.noreply.github.com>
2024-02-02 19:08:33 +0000

    docs: adds copyright notice

--- include/ashttp3lib/h3/http3-server.cpp
+++ include/ashttp3lib/h3/http3-server.cpp
@@ -1,3 +1,9 @@
+// Copytight (C) 2024, Ayush Anand
+// This library has adopted many parts of Cloudflare's Quiche library
+// and ethrefore we are including the license of the Quiche library
+// in this file as well. The Quiche library is licensed under the
+// BSD-2-Clause license and the license is included below.
+//
 // Copyright (C) 2018-2019, Cloudflare, Inc.
 // All rights reserved.
 //
